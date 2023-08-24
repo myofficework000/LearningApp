43fdedee8f03617232fd2ad9a9a3987f317f78d4
ABHISHEK PATHAK <51234843+myofficework000@users.noreply.github.com>
2023-08-25 01:20:25 +0530

    Added lottie animation

---
+++ app/src/main/java/com/example/learningapp/ui/theme/DecideTheme.kt
@@ -0,0 +1,10 @@
+package com.example.learningapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun decideTheme(theme: DynamicThemes?) = theme ?: (
+        if (isSystemInDarkTheme()) DynamicThemes.DEFAULT_DARK
+        else DynamicThemes.DEFAULT_LIGHT
+        )
