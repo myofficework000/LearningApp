8ea0d3c1e639e84e9f3aa7a80ada565d21b6a04f
PavaniVelma <pavanireddyvelma@gmail.com>
2023-08-24 17:33:15 -0400

    deleted image pngs and used existing images

--- app/src/main/java/com/example/learningapp/IntroScreen1.kt
+++ app/src/main/java/com/example/learningapp/IntroScreen1.kt
@@ -4,22 +4,29 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.learningapp.ui.theme.Green40
 import com.example.learningapp.ui.theme.grey
 
@@ -32,16 +39,35 @@ fun IntroScreen(){
 
         val guideline = createGuidelineFromTop(0.2f)
 
-        val(imageView, textDesc,buttonStarted, buttonOutline) = createRefs()
+        val(imageView,imageTitle,textDesc,buttonStarted, buttonOutline) = createRefs()
 
-        Image(painter = painterResource(id = R.drawable.img), contentDescription ="nothing",
-            modifier = Modifier.constrainAs(imageView){
+        val rawComposition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.animation_owl))
+        val progress by animateLottieCompositionAsState(composition = rawComposition)
+
+        LottieAnimation(
+            composition = rawComposition,
+            modifier = Modifier.size(220.dp).constrainAs(imageView){
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
                 top.linkTo(guideline)
-            }, contentScale = ContentScale.Fit )
+            }, contentScale = ContentScale.Inside,
+            iterations = 500
+        )
+        Text(text = stringResource(id = R.string.intro_title),
+            textAlign = TextAlign.Center,
+            color = Green40,
+            fontSize = 38.sp,
+            fontWeight = FontWeight.ExtraBold,
+            fontFamily = FontFamily.SansSerif,
+            modifier = Modifier
+                .padding(top = 16.dp, end = 16.dp, start = 16.dp)
+                .constrainAs(imageTitle) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    top.linkTo(imageView.bottom)
+                })
 
-        Text(text = stringResource(id = R.string.text),
+        Text(text = stringResource(id = R.string.intro_text),
             textAlign = TextAlign.Center,
             color = Color.Gray,
             fontSize = 24.sp,
@@ -50,7 +76,7 @@ fun IntroScreen(){
                 .constrainAs(textDesc) {
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-                    top.linkTo(imageView.bottom)
+                    top.linkTo(imageTitle.bottom)
                 })
 
         Button(onClick = { /*TODO*/ },
