1a70bef4036a8ee77205a1aaad7213eb35553b88
hushiyan <hushiyan01@gmail.com>
2023-08-25 16:41:24 -0400

    added how_many_words_to_learn_screen and sign_in_with_email_screen

--- app/src/main/java/com/example/learningapp/view/screen/HowManyWordsToLearnScreen.kt
+++ app/src/main/java/com/example/learningapp/view/screen/HowManyWordsToLearnScreen.kt
@@ -1,4 +1,80 @@
 package com.example.learningapp.view.screen
 
-class HowManyWordsToLearnScreen {
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.navigation.NavController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.learningapp.R
+import com.example.learningapp.view.theme.Green100
+
+@Composable
+fun HowManyWordsToLearn(navController: NavController){
+    ConstraintLayout(
+        Modifier.fillMaxSize()
+    ) {
+        val ( animation, hintText, btnContinue) = createRefs()
+        val guidelineTop = createGuidelineFromTop(0.2f)
+        val rawComposition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.animation_owl))
+
+        LottieAnimation(
+            composition = rawComposition,
+            modifier = Modifier
+                .size(150.dp)
+                .constrainAs(animation) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(guidelineTop)
+                    start.linkTo(parent.start)
+                },
+            contentScale = ContentScale.FillBounds,
+            iterations = Int.MAX_VALUE
+        )
+
+        Text(
+            text = "That's 25 words in your first week!",
+            modifier = Modifier
+                .size(50.dp)
+                .padding(20.dp)
+                .constrainAs(hintText) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(guidelineTop)
+                    end.linkTo(parent.end)
+                    start.linkTo(animation.end)
+                    width = Dimension.fillToConstraints
+                    height = Dimension.fillToConstraints
+                }
+        )
+
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(Green100),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+                .constrainAs(btnContinue) {
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }) {
+            Text(
+                text = stringResource(id = R.string.btn_continue),
+                color = Color.White
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/learningapp/view/screen/SignInEmailScreen.kt
+++ app/src/main/java/com/example/learningapp/view/screen/SignInEmailScreen.kt
@@ -1,4 +1,203 @@
 package com.example.learningapp.view.screen
 
-class SignInEmailScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material.icons.outlined.Clear
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.navigation.NavController
+import com.example.learningapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignInEmailScreen(navController: NavController) {
+    var userName by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isPwdVisible by remember { mutableStateOf(false) }
+    ConstraintLayout(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        val (staticText, areaInfo, forgotPwdButton, signInButton, googleButton, facebookButton) = createRefs()
+
+        Text(
+            fontSize = 25.sp,
+            color = Color.DarkGray,
+            modifier = Modifier
+                .constrainAs(staticText) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+            text = "Enter your details"
+        )
+
+        Column(
+            Modifier.constrainAs(areaInfo) {
+                top.linkTo(staticText.bottom, 20.dp)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+        ) {
+            TextField(
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.LightGray
+                ),
+                value = userName,
+                onValueChange = { userName = it },
+                label = { Text("Username or email") },
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            TextField(
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.LightGray
+                ),
+                modifier = Modifier.fillMaxWidth(),
+                value = password,
+                onValueChange = { password = it },
+                visualTransformation = (if (isPwdVisible) VisualTransformation.None else PasswordVisualTransformation()),
+                label = { Text("Password") },
+                trailingIcon = {
+                    IconButton(onClick = { isPwdVisible = !isPwdVisible }) {
+                        if (isPwdVisible) {
+                            Icon(imageVector = Icons.Outlined.Check, contentDescription = "")
+                        } else {
+                            Icon(imageVector = Icons.Outlined.Clear, contentDescription = "")
+                        }
+                    }
+                }
+            )
+        }
+
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Gray,
+                contentColor = Color.DarkGray
+            ),
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(signInButton) {
+                    top.linkTo(areaInfo.bottom, 16.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }) {
+            Text("SIGN IN")
+        }
+
+        TextButton(
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Blue
+            ),
+            onClick = {},
+            modifier = Modifier.constrainAs(forgotPwdButton) {
+                top.linkTo(signInButton.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+        ) {
+            Text(text = "FORGOT PASSWORD")
+        }
+
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Black
+            ),
+            onClick = { /* Handle Google login */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(
+                    width = 1.dp,
+                    color = Color.Gray,
+                    shape = RoundedCornerShape(10.dp) // Define rounded corners here
+                )
+                .constrainAs(googleButton) {
+                    bottom.linkTo(facebookButton.top, 16.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_google),
+                    contentDescription = "Google Logo",
+                    modifier = Modifier
+                        .size(24.dp)
+                        .padding(end = 8.dp)
+                )
+                Text("SIGN IN WITH GOOGLE")
+            }
+        }
+
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Black
+            ),
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(
+                    width = 1.dp,
+                    color = Color.Gray,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .constrainAs(facebookButton) {
+                    bottom.linkTo(parent.bottom, 16.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_facebook),
+                    contentDescription = "Facebook Logo",
+                    modifier = Modifier
+                        .size(24.dp)
+                        .padding(end = 8.dp)
+                )
+                Text("SIGN IN WITH FACEBOOK")
+            }
+        }
+
+    }
 }
