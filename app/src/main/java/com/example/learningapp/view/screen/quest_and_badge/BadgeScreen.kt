087b1727f69791933928b4a2ffd13a828ee04af7
Huu Duc Nguyen <ducnrg@gmail.com>
2023-08-28 22:58:37 -0400

    init Quest And Badge Screen

---
+++ app/src/main/java/com/example/learningapp/view/screen/quest_and_badge/BadgeScreen.kt
@@ -0,0 +1,7 @@
+package com.example.learningapp.view.screen.quest_and_badge
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BadgeScreen() {
+}
---
+++ app/src/main/java/com/example/learningapp/view/screen/quest_and_badge/QuestScreen.kt
@@ -0,0 +1,8 @@
+package com.example.learningapp.view.screen.quest_and_badge
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun QuestScreen(){
+
+}
